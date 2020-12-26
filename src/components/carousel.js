9870634f263387c9b738a34fdbf32209f6caaaa3
jnchaitanya077 <55684213+jnchaitanya077@users.noreply.github.com>
2020-12-26 21:49:20 +0530

    changed carousel to dynamic

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import uniqid from "uniqid";
 import "../css/header.css";
 import slider1 from "../images/slider/slider1.jpg";
 import slider_2 from "../images/slider/slider_2.jpg";
@@ -7,6 +8,15 @@ import slider_4 from "../images/slider/slider_4.jpg";
 import slider_5 from "../images/slider/slider_7.jpg";
 
 function App() {
+  const [images, setImages] = useState({ images: [] });
+
+  // load carousel form database
+  useEffect(() => {
+    fetch("http://localhost:5000/loadImagesApi")
+      .then((data) => data.json())
+      .then((data) => setImages(data));
+  }, []);
+
   return (
     <section>
       <div
@@ -16,20 +26,19 @@ function App() {
       >
         <div className="carousel-inner">
           <div className="carousel-item active c-item" data-pause="hover">
-            <img src={slider1} className="d-block w-100 " alt="slider-image" />
+            <img src={slider1} className="d-block w-100 " alt="slider-image1" />
           </div>
-          <div className="carousel-item c-item">
-            <img src={slider_2} className="d-block w-100" alt="slider-image" />
-          </div>
-          <div className="carousel-item c-item">
-            <img src={slider_3} className="d-block w-100" alt="slider-image" />
-          </div>
-          <div className="carousel-item c-item">
-            <img src={slider_4} className="d-block w-100" alt="slider-image" />
-          </div>
-          <div className="carousel-item c-item">
-            <img src={slider_5} className="d-block w-100" alt="slider-image" />
-          </div>
+          {images.images.map((eachImage) => {
+            return (
+              <div key={uniqid()} className="carousel-item c-item">
+                <img
+                  src={"http://localhost:5000/" + eachImage}
+                  className="d-block w-100"
+                  alt="slider-image1"
+                />
+              </div>
+            );
+          })}
         </div>
         <a
           className="carousel-control-prev"
